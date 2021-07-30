c4b0b3f6f8339cbfadcb66ebf44e41d646fac73c
Shiv Kumar Yadav <53552190+sky2030@users.noreply.github.com>
2021-07-30 13:15:09 +0530

    Update Keys.js

--- Keys.js
+++ Keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-    CytadbUrl:"mongodb+srv://cyta:tenant@cluster0.k5d5q.mongodb.net/cytaDB?retryWrites=true&w=majority",
-    mongoUrl:"mongodb+srv://skyadav:shivsky7@cluster0.xdcel.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    jwtkey:"any random key "
+  
 }
